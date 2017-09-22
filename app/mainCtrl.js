2b372b9cf1219765df248c080f79517976de4602
Brandon Gagon <jirishus@yahoo.com>
2017-09-22 09:16:10 -0700

    Removal of commented-out code
    removal of commented-out code for better readability and consistency

--- app/mainCtrl.js
+++ app/mainCtrl.js
@@ -2,14 +2,12 @@ app.controller('mainCtrl', function($scope,$http,Resty) {
   
   $scope.searchForm = {};
 
-  $scope.formSubmit = function(searchForm) {
-    // $scope.searchForm = {};
+  $scope.formSubmit = function(searchForm) {   
     let userInput = searchForm.searchText;
 
     Resty
     .getResource(userInput)
-    .then((payload) => {
-      // $scope.imagePath = payload.data.response.results[0].blocks.main.elements[0].assets[0].file;
+    .then((payload) => {      
       $scope.stories = payload.data.response.results;
     });
   }
