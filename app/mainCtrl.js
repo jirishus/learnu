01b564d4332567f7db0ffe1bf88180fb408d196a
Brandon Gagon <jirishus@yahoo.com>
2021-05-28 17:02:34 -0700

    Develop (#1)
    * loader and result not found detection
    
    * actions pipeline
    
    Co-authored-by: brandon gagon <brandongagon@brandons-Air.attlocal.net>

--- app/mainCtrl.js
+++ app/mainCtrl.js
@@ -1,14 +1,26 @@
-app.controller('mainCtrl', function($scope,$http,Resty) {
+app.controller('mainCtrl', function($scope, Resty, $timeout) {
   
   $scope.searchForm = {};
+  $scope.isLoading = false;
+  $scope.resultsFound = false;
 
   $scope.formSubmit = function(searchForm) {   
+    // debugger;
     let userInput = searchForm.searchText;
+    $scope.isLoading = true;
+    $scope.stories = [];
 
     Resty
     .getResource(userInput)
-    .then((payload) => {      
-      $scope.stories = payload.data.response.results;
+    .then((payload) => {                 
+      // $scope.stories = payload.data.response.results;            
+      $timeout(function () {
+        console.log('Results Successfully Loaded');
+        $scope.isLoading = false;
+        $scope.resultsFound = true;
+        $scope.stories = (payload.data.response.results.length > 0) ? payload.data.response.results : [{ webTitle: 'No Results Found' }];            
+      }, 3000);
+      
     });
   }
 
